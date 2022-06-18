900096bcb434ae504f0f5be69959ce35aa3d0a03
Devin Wingo <devinwingo@gmail.com>
2022-06-17 18:44:03 -0700

    deleted unused annotations

--- src/main/java/com/devinwingo/capstone/controllers/HomeController.java
+++ src/main/java/com/devinwingo/capstone/controllers/HomeController.java
@@ -4,12 +4,12 @@ import com.devinwingo.capstone.models.Post;
 import com.devinwingo.capstone.services.PostService;
 import com.devinwingo.capstone.services.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Collection;
+
 
 @Controller
 @Slf4j
