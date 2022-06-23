ea22548fbcf7b0f31f9d5cadde907b3fe8dc319b
Devin Wingo <devinwingo@gmail.com>
2022-06-22 18:25:13 -0700

    moved methods from usercontroller to admin controller. my_posts view, with controller method to show 'user profile'. create post controller is working with some hardcoded test values. most user specific info will not be hardcoded when security is implemented

--- src/main/java/com/devinwingo/capstone/dao/AuthGroupRepository.java
+++ src/main/java/com/devinwingo/capstone/dao/AuthGroupRepository.java
@@ -1,5 +1,14 @@
 package com.devinwingo.capstone.dao;
 
-public interface AuthGroupRepository
-{
+import com.devinwingo.capstone.models.AuthGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthGroupRepository extends JpaRepository<AuthGroup, Integer> {
+
+    //select * from auth_group where aemail = :email
+    List<AuthGroup> findByaEmail(String email);
 }
--- src/main/java/com/devinwingo/capstone/models/AuthGroup.java
+++ src/main/java/com/devinwingo/capstone/models/AuthGroup.java
@@ -1,4 +1,31 @@
 package com.devinwingo.capstone.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Slf4j
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "auth_group")
+@Entity
 public class AuthGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    //User Key
+    @NonNull
+    String aEmail;
+
+    //User Role
+    @NonNull
+    String aAuthGroup;
 }
